7b2e9e12ab2e253bf22fbe72f867ad5b1a877cf0
quinnhenschel <qhenschel@gmail.com>
2016-12-19 21:30:05 -0500

    Added Screen Resize
    resize screen to monitor height and width

--- BaseGame99/Main.cpp
+++ BaseGame99/Main.cpp
@@ -13,6 +13,25 @@ void InitAllegro(int W, int H)
 		printf("failed to initialize allegro!\n");
 	}
 
+
+	ALLEGRO_DISPLAY       *display = NULL;
+	ALLEGRO_DISPLAY_MODE   disp_data;
+
+	al_init(); // I'm not checking the return value for simplicity.
+	al_init_image_addon();
+	al_init_primitives_addon();
+
+	al_get_display_mode(0, &disp_data);
+
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	display = al_create_display(disp_data.width, disp_data.height);
+
+	al_rest(3);
+	al_destroy_display(display);
+
+
+
+
 	//initialize display screen
 	display = al_create_display(W, H);
 	if (!display)
@@ -22,8 +41,10 @@ void InitAllegro(int W, int H)
 	}
 	else
 	{
+		al_resize_display(display, disp_data.width, disp_data.height);
 		printf("ok");
 		al_clear_to_color(al_map_rgb(0, 0, 0));
+
 	}
 
 	//initialize keyboard
